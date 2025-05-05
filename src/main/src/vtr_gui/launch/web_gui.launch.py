21b2e82689115eb03df8c55100104722ca620302
adam <adamsa@mweb.co.za>
2025-05-05 13:49:13 +0200

    add launch file

---
+++ src/main/src/vtr_gui/launch/web_gui.launch.py
@@ -0,0 +1,35 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    """Launch web-gui, socket-server, and socket-client."""
+   # Define your three nodes
+    node1 = Node(
+        package='vtr_gui',
+        executable='web_server',
+        name='web_server',
+        output='screen'
+    )
+    
+    node2 = Node(
+        package='vtr_gui',
+        namespace='a200_0656/vtr',
+        executable='socket_server',
+        name='socket_server',
+        output='screen'
+    )
+    
+    node3 = Node(
+        package='vtr_gui',
+        namespace='a200_0656/vtr',
+        executable='socket_client',
+        name='socket_client',
+        output='screen'
+    )
+    
+    # Return the launch description with all three nodes
+    return LaunchDescription([
+        node1,
+        node2,
+        node3
+    ])
--- src/main/src/vtr_gui/setup.py
+++ src/main/src/vtr_gui/setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+import os
 from glob import glob
 
 package_name = 'vtr_gui'
@@ -23,6 +24,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
