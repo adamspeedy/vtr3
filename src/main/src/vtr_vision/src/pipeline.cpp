c342fcaefbf6da7a0031745b6823567c31bf38fb
ARU Robots <aru.robots@gmail.com>
2025-05-29 00:11:07 +0200

    cleaning output

--- src/main/src/vtr_vision/src/pipeline.cpp
+++ src/main/src/vtr_vision/src/pipeline.cpp
@@ -74,15 +74,15 @@ void StereoPipeline::runOdometry_(const tactic::QueryCache::Ptr &qdata0, const t
  
   qdata.T_r_m.emplace(*qdata.T_r_v_odo);
   qdata.T_r_m_prior.emplace(*qdata.T_r_v_odo);
-  CLOG(WARNING, "stereo.pipeline") << "T_r_v_odo set";
+  CLOG(INFO, "stereo.pipeline") << "T_r_v_odo set";
 
-  CLOG(WARNING, "stereo.pipeline") << "First frame: " << *qdata.first_frame ;
+  CLOG(INFO, "stereo.pipeline") << "First frame: " << *qdata.first_frame ;
 
   if (!(*qdata.first_frame)){
     qdata.timestamp_odo.emplace(timestamp_odo_);
     setOdometryPrior(qdata, graph);
   }
-  CLOG(WARNING, "stereo.pipeline") << "Finished setting odometry prior, running modules";
+  CLOG(INFO, "stereo.pipeline") << "Finished setting odometry prior, running modules";
   for (auto module : odometry_) module->run(*qdata0, *output0, graph, executor);
 
   // If VO failed, revert T_r_m to the initial prior estimate
